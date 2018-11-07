d9c6b88073c8c444b0654167d356c59b517fa28f
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-07 17:48:13 +0900

    Connpass からJson を取得できる

--- meetups-api/meetupsApi.Tests/Repository/ConpassClientTest.cs
+++ meetups-api/meetupsApi.Tests/Repository/ConpassClientTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace meetupsApi.Tests.Repository
@@ -13,10 +15,24 @@ namespace meetupsApi.Tests.Repository
             var client = new ConpassClient();
             Assert.NotNull(client);
         }
+
+        [Fact]
+        async void ConnpassからJsonを取得することができる()
+        {
+            var client = new ConpassClient();
+            var json = await client.loadJsonAwait();
+            Assert.NotNull(json);
+        }
     }
 
     public class ConpassClient
     {
-
+        public async Task<object> loadJsonAwait()
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.GetAsync("https://connpass.com/api/v1/event/");
+            }
+        }
     }
 }
