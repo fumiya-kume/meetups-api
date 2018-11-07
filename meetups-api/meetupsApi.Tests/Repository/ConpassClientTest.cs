2628704728a2695e0571fd3d73b58bdc939ef44c
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-07 17:43:00 +0900

    ConnpassClient を作成した

---
+++ meetups-api/meetupsApi.Tests/Repository/ConpassClientTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace meetupsApi.Tests.Repository
+{
+    public class ConpassClientTest
+    {
+        [Fact]
+        void Connpassにアクセスするクラスが存在する()
+        {
+            var client = new ConpassClient();
+            Assert.NotNull(client);
+        }
+    }
+
+    public class ConpassClient
+    {
+
+    }
+}
