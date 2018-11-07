543f1b30e7a41b3af832b41cf56dc67f78579d26
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-07 19:18:07 +0900

    テストプロジェクトから本番プロジェクトに移動した

--- meetups-api/meetupsApi.Tests/Repository/ConpassClientTest.cs
+++ meetups-api/meetupsApi.Tests/Repository/ConpassClientTest.cs
@@ -1,32 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-using meetupsApi.JsonEntity;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace meetupsApi.Tests.Repository
 {
     public class ConpassClientTest
     {
-        [Fact]
-        void Connpassにアクセスするクラスが存在する()
-        {
-            var client = new ConnpassClient();
-            Assert.NotNull(client);
-        }
-
-        [Fact]
-        async void ConnpassからJsonを取得することができる()
-        {
-            var client = new ConnpassClient();
-            var json = await client.loadJsonAwait();
-            Assert.NotNull(json);
-            Assert.NotEmpty(json);
-        }
-
         [Fact]
         async void Connpassから受信したデータをパースすることができる()
         {
@@ -43,27 +23,5 @@ namespace meetupsApi.Tests.Repository
             Assert.Equal(99, connpassData.events.Length);
         }
 
-    }
-
-    public class ConnpassClient
-    {
-        public async Task<string> loadJsonAwait(int capacity = 100)
-        {
-            using (var client = new HttpClient())
-            {
-                var response = await client.GetAsync($"https://connpass.com/api/v1/event/?count={capacity}");
-                return await response.Content.ReadAsStringAsync();
-            }
-        }
-
-        public async Task<ConnpassMeetupJson> LoadConnpassDataAsync(int capacity = 100)
-        {
-            var json = await loadJsonAwait(capacity);
-            if (string.IsNullOrEmpty(json))
-            {
-                return null;
-            }
-            return JsonConvert.DeserializeObject<ConnpassMeetupJson>(json);
-        }
     }
 }
