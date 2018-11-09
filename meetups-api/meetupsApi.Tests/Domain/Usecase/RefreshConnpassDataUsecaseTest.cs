edf2219197e82249babc98205db63ce384d4c21e
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-09 14:08:11 +0900

    ユースケースが実行されたらリポジトリが更新される

--- meetups-api/meetupsApi.Tests/Domain/Usecase/RefreshConnpassDataUsecaseTest.cs
+++ meetups-api/meetupsApi.Tests/Domain/Usecase/RefreshConnpassDataUsecaseTest.cs
@@ -8,10 +8,13 @@ namespace meetupsApi.Tests.Domain.Usecase
     public class RefreshConnpassDataUsecaseTest
     {
         [Fact]
-        void コンパスのデータRepositoryを受け取り処理を進める()
+        void ユースケースが実行されたらコンパスのデータを更新する()
         {
             Mock<IConnpassDataRepository> connpassDataRepositoryMoq = new Mock<IConnpassDataRepository>();
+            connpassDataRepositoryMoq.Setup(obj => obj.RefreshData());
             var usecase = new RefreshConnpassDataUsecase(connpassDataRepositoryMoq.Object);
+            usecase.execute();
+            connpassDataRepositoryMoq.Verify(obj => obj.RefreshData(), Times.Once);
         }
     }
 
@@ -33,6 +36,7 @@ namespace meetupsApi.Tests.Domain.Usecase
 
         public void execute()
         {
+            _connpassDataRepository.RefreshData();
         }
     }
 }
