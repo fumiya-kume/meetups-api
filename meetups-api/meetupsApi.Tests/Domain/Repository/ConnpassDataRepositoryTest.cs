310057b704d2a3eb6e82fe4b22071ec6410ffd90
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-09 15:48:28 +0900

    データを更新するとデータストアからデータを取得してくる

--- meetups-api/meetupsApi.Tests/Domain/Repository/ConnpassDataRepositoryTest.cs
+++ meetups-api/meetupsApi.Tests/Domain/Repository/ConnpassDataRepositoryTest.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
+using meetupsApi.JsonEntity;
 using meetupsApi.Tests.Domain.Usecase;
+using Moq;
 using Xunit;
 
 namespace meetupsApi.Tests.Domain.Repository
@@ -8,23 +11,48 @@ namespace meetupsApi.Tests.Domain.Repository
         [Fact]
         void ConnpassDataRepositoryが存在する()
         {
-            ConnpassDataRepository connpassDataRepository = new ConnpassDataRepository();
+            var dataStoreMoq = new Mock<IConnpassDataStore>();
+            var connpassDataRepository = new ConnpassDataRepository(dataStoreMoq.Object);
         }
 
         [Fact]
         void ConnpassDataRepositoryはIConnpassDataRepositoryを実装する()
         {
-            var connpassDataRepository = new ConnpassDataRepository();
+            var dataStoreMoq = new Mock<IConnpassDataStore>();
+            var connpassDataRepository = new ConnpassDataRepository(dataStoreMoq.Object);
             var actual = connpassDataRepository as IConnpassDataRepository;
             Assert.NotNull(actual);
         }
+
+        [Fact]
+        void データを更新するとデータストアからデータを取得してくる()
+        {
+            var dataStoreMoq = new Mock<IConnpassDataStore>();
+            var dummyConnpassData = new ConnpassMeetupJson();
+            dataStoreMoq.Setup(obj => obj.LoadConnpassDataAsync(100)).ReturnsAsync(dummyConnpassData);
+            var connpassDataRepository = new ConnpassDataRepository(dataStoreMoq.Object);
+            connpassDataRepository.RefreshData();
+            dataStoreMoq.Verify(obj => obj.LoadConnpassDataAsync(100), Times.Once);
+        }
     }
 
-    public class ConnpassDataRepository:IConnpassDataRepository
+    public interface IConnpassDataStore
     {
+        Task<ConnpassMeetupJson> LoadConnpassDataAsync(int capacity = 100);
+    }
+
+    public class ConnpassDataRepository : IConnpassDataRepository
+    {
+        private IConnpassDataStore _connpassDataStore;
+
+        public ConnpassDataRepository(IConnpassDataStore dataStore)
+        {
+            _connpassDataStore = dataStore;
+        }
+
         public void RefreshData()
         {
-            throw new System.NotImplementedException();
+            _connpassDataStore.LoadConnpassDataAsync(100);
         }
     }
 }
