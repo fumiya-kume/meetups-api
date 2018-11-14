1f94659b2244cf8e203b7c26e1d50efd6f4bf35a
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-14 17:46:49 +0900

    json のプロパティ名を明示的につけるようにした

--- meetups-api/meetups-api/Infra/JsonEntity/ConnpassMeetupJson.cs
+++ meetups-api/meetups-api/Infra/JsonEntity/ConnpassMeetupJson.cs
@@ -1,8 +1,11 @@
-﻿namespace meetupsApi.JsonEntity
+﻿using Newtonsoft.Json;
+
+namespace meetupsApi.JsonEntity
 {
     public class ConnpassMeetupJson
     {
         public int results_returned { get; set; }
+        [JsonProperty("events")] 
         public ConnpassEvent[] ConnpassEvents { get; set; }
         public int results_start { get; set; }
         public int results_available { get; set; }
