6933e3b6e125e7c5cf6a48c6906897fc1d984491
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-23 23:30:28 +0900

    保存できるConnpassのデータを増やした

--- meetups-api/meetups-api/Domain/Entity/ConnpassEventDataEntity.cs
+++ meetups-api/meetups-api/Domain/Entity/ConnpassEventDataEntity.cs
@@ -1,5 +1,7 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Permissions;
 
 namespace meetupsApi.Domain.Entity
 {
@@ -7,8 +9,21 @@ namespace meetupsApi.Domain.Entity
     {
         public int Id { get; set; }
         public string title { get; set; }
+        public string catchMesagge { get; set; }
         public string description { get; set; }
         public string event_url { get; set; }
+        public string hash_tag { get; set; }
+        public DateTime started_at { get; set; }
+        public DateTime ended_at { get; set; }
+        public int limit { get; set; }
+        public string event_type { get; set; }
+        public string address { get; set; }
+        public int owned_id { get; set; }
+        public string owned_nickname { get; set; }
+        public string owner_display_name { get; set; }
+        public int accepted { get; set; }
+        public int waiting { get; set; }
+        public DateTime updated_at { get; set; }
         public double Lon { get; set; }
         public double Lat { get; set; }
     }
--- meetups-api/meetups-api/Migrations/20181123142951_addConnpassDataColumn.Designer.cs
+++ meetups-api/meetups-api/Migrations/20181123142951_addConnpassDataColumn.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using meetupsApi.Models;
 
 namespace meetupsApi.Migrations
 {
     [DbContext(typeof(MeetupsApiContext))]
-    partial class meetupsApiContextModelSnapshot : ModelSnapshot
+    [Migration("20181123142951_addConnpassDataColumn")]
+    partial class addConnpassDataColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
